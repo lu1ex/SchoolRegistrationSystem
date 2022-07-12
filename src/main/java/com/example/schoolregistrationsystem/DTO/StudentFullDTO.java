750c1ed08cd695ba5c10905a883621739fb0cd23
Rafal Suski <rsuski7@wp.pl>
2022-07-12 18:11:42 +0200

    Added two DTO's for objects in many to many relationship - fixed problem with toString operation

---
+++ src/main/java/com/example/schoolregistrationsystem/DTO/StudentFullDTO.java
@@ -0,0 +1,22 @@
+package com.example.schoolregistrationsystem.DTO;
+
+import com.example.schoolregistrationsystem.entity.CourseEntity;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentFullDTO {
+    private String name;
+    private String surname;
+    private LocalDate dateOfBirth;
+    private String phoneNumber;
+    private String studentCardID;
+    private Set<CourseEntity> courses = new HashSet<>();
+}
